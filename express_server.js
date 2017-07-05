9a697f43edd8413a793ce146176e33142197d56a
Yelim Oh <yelim.oh1@gmail.com>
2017-07-05 04:33:03 +0000

    Added redirection under /:shortURL with if/else case

--- express_server.js
+++ express_server.js
@@ -14,16 +14,13 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/url/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id,
-                       longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -33,10 +30,24 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
-  res.redirect(`http://localhost:8080/urls/${randomString}`);
   console.log(req.body);  // debug statement to see POST parameters
   console.log(urlDatabase);
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${randomString}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if(longURL) {
+    res.redirect(longURL);
+  } else {
+  res.end('301 Moved Permanently.');
+  }
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
