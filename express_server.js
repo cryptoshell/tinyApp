6fdb278fa9712b3637b48b42e9f703a5fdaeef3a
Yelim Oh <yelim.oh1@gmail.com>
2017-07-07 23:30:40 +0000

    Corrected redirection bug and added encrypted cookies using cookieSession

--- express_server.js
+++ express_server.js
@@ -4,31 +4,24 @@ const express = require("express");
 const app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "h2lm1p": { longURL: "http://www.lighthouselabs.ca",
-                owner: "a@a.com"
-            }
-};
+const urlDatabase = {};
 
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
+const users = {};
 
 function generateRandomString() {
   let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -48,20 +41,28 @@ function userByEmail(email) {
   }
 }
 
+function httpChecker(longURL) {
+  if (longURL.includes("http://")) {
+    return longURL;
+  } else {
+    return ("http://" + longURL);
+  }
+}
+
 // ROUTES
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase,
-                       user: users[req.cookies["user_id"]] };
+  let templateVars = { urls: urlDatabase[req.session.user_id],
+                       user: users[req.session.user_id] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  if (users[req.cookies["user_id"]]) {
-    let templateVars = { user: users[req.cookies["user_id"]] };
+  if (users[req.session.user_id]) {
+    let templateVars = { user: users[req.session.user_id] };
     res.render("urls_new", templateVars);
   } else {
     res.redirect("/login");
@@ -69,33 +70,46 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  const creator = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = {longURL: req.body.longURL, owner: creator};
+  if (urlDatabase[userID] === undefined) {
+    urlDatabase[userID] = {};
+    urlDatabase[userID][shortURL] = req.body.longURL;
+  } else {
+    urlDatabase[userID][shortURL] = req.body.longURL;
+  }
   console.log(req.body);  // debug statement to see POST parameters
   console.log(urlDatabase);
   res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  if(longURL === undefined) {
-    res.sendStatus(301);
+  let longURL;
+  for (let user in urlDatabase) {
+    longURL = urlDatabase[user][req.params.shortURL];
+    if(longURL) {
+      break;
+    }
+  }
+  if (longURL) {
+    res.redirect(httpChecker(longURL));
   } else {
-    res.redirect(longURL);
+    res.sendStatus(404);
   }
 });
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                        longURL: urlDatabase[req.params.id],
-                       user: users[req.cookies["user_id"]] };
+                       user: users[req.session.user_id] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  if(urlDatabase[req.params.id].owner === req.cookies["user_id"]) {
-    delete urlDatabase[req.params.id];
+  const userID = req.session.user_id;
+  const shortURL = req.params.id;
+  if(urlDatabase[userID][shortURL]) {
+    delete urlDatabase[userID][shortURL];
     res.redirect("/urls");
   } else {
     res.send("Not a creator of this URL!");
@@ -104,9 +118,11 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // Updated long URL using POST
 app.post("/urls/:id", (req, res) => {
-  if(urlDatabase[req.params.id].owner === req.cookies["user_id"]) {
-    urlDatabase[req.params.id].longURL = req.body.longURL;
-    console.log(req.body.longURL)
+  const userID = req.session.user_id;
+  const shortURL = req.params.id;
+  if(urlDatabase[userID][shortURL]) {
+    urlDatabase[userID][shortURL] = req.body.longURL;
+    console.log(req.body.longURL, req.body.longURL);
     res.redirect("/urls");
   } else {
     res.send("Not a creator, cannot edit!");
@@ -117,21 +133,21 @@ app.post("/login", (req, res) => {
   const user = userByEmail(req.body.email);
   if(!users[user]){
     res.status(403).send("403 - Forbidden <br> E-mail cannot be found!")
-  } else if (req.body.password !== users[user].password) {
+  } else if (!bcrypt.compareSync(req.body.password, users[user].password)) {
       res.status(403).send("403 - Forbidden <br> Incorrect password!")
   } else {
-    res.cookie('user_id', userByEmail(req.body.email));
+    req.session.user_id = userByEmail(req.body.email);
     res.redirect("/urls");
   }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session.user_id = null;
   res.redirect("/urls");
 });
 
 app.get("/register", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.user_id] };
   res.render("_register", templateVars);
 });
 
@@ -144,14 +160,15 @@ app.post("/register", (req, res) => {
     let userRandomID = generateRandomString();
     users[userRandomID] = { id: userRandomID,
                           email: req.body.email,
-                          password: req.body.password };
-    res.cookie("user_id", userRandomID);
+                          password: bcrypt.hashSync(req.body.password, 10) };
+    console.log(users);
+    req.session.user_id = userRandomID;
     res.redirect("/urls");
   }
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.user_id] };
   res.render("_login", templateVars);
 });
 
